4a45c3e264c16e0d7699105cda2618832dccbde7
zombcat <pumpkinzomb@gmail.com>
2024-09-08 17:09:00 +0900

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import YouTubeDownloader from "./components/YoutubeDownloader";
+
+function App() {
+  return (
+    <div className="App">
+      <YouTubeDownloader />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/YoutubeDownloader.tsx
@@ -0,0 +1,165 @@
+import React, { useState } from "react";
+import {
+  TextField,
+  Button,
+  Typography,
+  Box,
+  Card,
+  CardMedia,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Stack,
+} from "@mui/material";
+import axios from "axios";
+import { ReactComponent as Logo } from "../assets/YouTubeDownloaderLogo.svg";
+
+const validFormats = [
+  "mp4",
+  "webm",
+  "flv",
+  "ogg",
+  "mkv",
+  "mp3",
+  "m4a",
+  "wav",
+  "aac",
+];
+
+const YouTubeDownloader: React.FC = () => {
+  const [url, setUrl] = useState("");
+  const [thumbnail, setThumbnail] = useState("");
+  const [format, setFormat] = useState("mp4");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [downloadLink, setDownloadLink] = useState("");
+
+  const getDownloadUrl = async (url: string, format: string) => {
+    const response = await axios.post("/api/downloads", {
+      url,
+      format,
+    });
+    return response.data;
+  };
+
+  const extractVideoId = (url: string) => {
+    const regExp =
+      /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    const match = url.match(regExp);
+    return match && match[2].length === 11 ? match[2] : null;
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("");
+    setDownloadLink("");
+    const videoId = extractVideoId(url);
+    setThumbnail(videoId ? `https://img.youtube.com/vi/${videoId}/0.jpg` : "");
+
+    try {
+      const { downloadUrl } = await getDownloadUrl(url, format);
+      setDownloadLink(downloadUrl);
+    } catch (err) {
+      setError("An error occurred while processing your request.");
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Stack
+      sx={{
+        width: "100vw",
+        height: "100vh",
+      }}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Box sx={{ width: "500px" }}>
+        <Logo style={{ width: "500px", marginBottom: "20px" }} />
+        <form onSubmit={handleSubmit}>
+          <Stack flexDirection={"row"} gap={1}>
+            <TextField
+              fullWidth
+              label="YouTube URL"
+              variant="outlined"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+              margin="normal"
+              sx={{
+                minWidth: "400px",
+              }}
+            />
+            <FormControl fullWidth margin="normal">
+              <InputLabel>Format</InputLabel>
+              <Select
+                value={format}
+                label="Format"
+                onChange={(e) => setFormat(e.target.value)}
+              >
+                {validFormats.map((fmt) => (
+                  <MenuItem key={fmt} value={fmt}>
+                    {fmt}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Stack>
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            disabled={isLoading}
+            sx={{
+              mt: 1,
+              height: "60px",
+              backgroundColor: "#000000",
+              fontWeight: "700",
+              fontSize: "16px",
+            }}
+          >
+            {isLoading ? "Processing..." : "Download"}
+          </Button>
+        </form>
+        <Stack
+          flexDirection={"row"}
+          gap={2}
+          alignItems={"center"}
+          sx={{ marginTop: 2 }}
+        >
+          {thumbnail && (
+            <Card sx={{ m: "auto", width: "320px" }}>
+              <CardMedia component="img" image={thumbnail} />
+            </Card>
+          )}
+          {downloadLink && (
+            <Button
+              href={downloadLink}
+              variant="contained"
+              color="secondary"
+              fullWidth
+              sx={{
+                height: "60px",
+                backgroundColor: "#000000",
+                fontWeight: "700",
+                fontSize: "16px",
+              }}
+            >
+              Download Video
+            </Button>
+          )}
+        </Stack>
+        {error && (
+          <Typography color="error" sx={{ mt: 2, textAlign: "center" }}>
+            {error}
+          </Typography>
+        )}
+      </Box>
+    </Stack>
+  );
+};
+
+export default YouTubeDownloader;
